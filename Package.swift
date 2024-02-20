528d733dfdca5a04178c98d3753911dd7123bbab
Laurent Jeanjean <laurent.jeanjean@bnpparibas.com>
2024-02-20 17:40:20 +0100

    feat: Added comment to pacakge

--- Package.swift
+++ Package.swift
@@ -29,6 +29,7 @@ let package = Package(
     ),
     .binaryTarget(
       name: "swiftgen",
+      // We use gitlab-dogen to avoid proxy protection from downloading rawcontent of github
       url: "https://gitlab-dogen.group.echonet/dm/bddf/ap26093/mchb_ios/artifacts/-/raw/master/swiftgen.artifactbundle.zip",
       checksum: "b29bc733208ca8348778264dd4547d169d7a349b3a6844dd8aaa06d1cb276a17"
     )
